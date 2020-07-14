17523a524bf637cb95ba83ef724d9b3a5e7ee45e
Srikant <ssrikant13@yahoo.com>
2020-07-14 14:43:43 -0700

    Spring project upload

---
+++ src/main/java/com/live/repository/OracleRepository.java
@@ -0,0 +1,14 @@
+package com.live.repository;
+
+import java.util.List;
+
+import com.live.model.Product;
+
+public class OracleRepository  implements ProductRepository {
+@Override
+	public List<Product> getProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/live/service/ProductServiceImpl.java
@@ -0,0 +1,14 @@
+package com.live.service;
+
+import java.util.List;
+
+import com.live.model.Product;
+
+public class ProductServiceImpl implements ProductService{
+
+	//@Override
+	public List<Product> getProducts() {
+		return null;
+	}
+
+}
